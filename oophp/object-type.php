1842f8c4b0224f383b2e692642c5566e5f2ed232
HARTANTOO <nanankampusbsi94@yahoo.com>
2023-01-26 23:41:18 +0700

    menambahkan konsep oop inheritance

--- oophp/object-type.php
+++ oophp/object-type.php
@@ -4,32 +4,65 @@ class Produk
   public $judul,
     $penulis,
     $penerbit,
-    $harga;
+    $harga,
+    $jmlHalaman,
+    $waktuMain,
+    $tipe;
 
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe)
   {
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
     $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->waktuMain = $waktuMain;
+    $this->tipe = $tipe;
   }
 
   public function getLabel()
   {
     return "$this->penulis, $this->penerbit";
   }
-}
 
-class CetakInfoProduk{
-  public function cetak($produk){
-    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})"
+  //baut method baru
+  //komik : Naruto | Mashashi kisimoto, shonan jump (Rp. 30000) - 100 Halaman
+  public function getInfoLengkap()
+  {
+    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ";
+    if ($this->tipe == "Komik") {
+      $str .= "- {$this->jmlHalaman} Halaman.";
+    } else if ($this->tipe == "Game") {
+      $str .= " ~ {$this->waktuMain} Jam.";
+    }
+    return $str;
   }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 300000);
-$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
+class CetakInfoProduk
+{
+  public function cetak($produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+  }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 300000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50, "Game");
 
 
 echo " Komik = " . $produk1->getLabel();
 echo "<br>";
 echo " Game = " . $produk2->getLabel();
+echo "<br>";
+echo $produk1->getInfoLengkap();
+echo "<br>";
+echo $produk2->getInfoLengkap();
+
+
+
+//komik : Naruto | Mashashi kisimoto, shonan jump (Rp. 30000) - 100 Halaman
+//Game : Uncharted | Neil Druckmann, Sony Computer (Rp. 25000) -50jam
+
+
+//menit 5:06
